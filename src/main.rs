7990a0719fb6e891952ffc9412be767731cfb6ea
hhan87 <hhan87@gmail.com>
2024-01-22 00:08:01 +0900

    add chmod and change dir owner

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs::{self, DirEntry};
 use std::io;
 use std::path::Path;
+use nix::sys::stat::Mode;
+use nix::sys::stat::fchmodat;
 use nix::unistd::{chown};
 use nix::unistd::User;
 use nix::unistd::Group;
@@ -13,12 +15,11 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<i64> {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                println!("Processing path: {}", path.display());
+                println!("Processing directory: {}", path.display());
                 count += visit_dirs(&path, cb)?;
-            } else {
-                count += 1;
-                cb(&entry);
             }
+            cb(&entry);
+            count += 1;
         }
     }
     Ok(count)
@@ -27,14 +28,15 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<i64> {
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 4 {
-        eprintln!("Usage: {} <owner_name> <group_name> <directory>", args[0]);
+    if args.len() != 5 {
+        eprintln!("Usage: {} <owner_name> <group_name> <chmod_value> <directory>", args[0]);
         std::process::exit(1);
     }
 
     let owner_name = &args[1];
     let group_name = &args[2];
-    let root_dir = Path::new(&args[3]);
+    let chmod_value = u32::from_str_radix(&args[3], 8).expect("Invalid chmod value");
+    let root_dir = Path::new(&args[4]);
 
     let owner_uid = User::from_name(owner_name).expect("Failed to find user")
         .expect("No such user")
@@ -45,10 +47,13 @@ fn main() -> io::Result<()> {
         .gid;
 
     let count = visit_dirs(&root_dir, &|entry: &DirEntry| {
-        chown(entry.path().as_path(), Some(owner_uid), Some(group_gid))
+        let path = entry.path();
+        chown(path.as_path(), Some(owner_uid), Some(group_gid))
             .expect("Failed to change ownership");
+        fchmodat(None, path.as_path(), Mode::from_bits_truncate(chmod_value), nix::sys::stat::FchmodatFlags::NoFollowSymlink)
+
+            .expect("Failed to change file mode");
     })?;
     println!("Total processed file count : {}", count);
     Ok(())
 }
-
